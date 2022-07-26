d5226afd15f276db396d8e900cdeb9867c4ceea3
lumicia <zuoshengxie@outlook.com>
2022-07-26 11:28:39 +0800

    add p0455

---
+++ src/greedy/mod.rs
@@ -0,0 +1,2 @@
+pub mod p0135_candy;
+pub mod p0455_assign_cookies;
---
+++ src/greedy/p0455_assign_cookies.rs
@@ -0,0 +1,28 @@
+pub fn find_content_children(children: &mut Vec<i32>, cookies: &mut Vec<i32>) -> u32 {
+    children.sort();
+    cookies.sort();
+    let mut child = 0;
+    let mut cookie = 0;
+
+    while child < children.len() && cookie < cookies.len() {
+        if children[child] <= cookies[cookie] {
+            child += 1;
+        }
+
+        cookie += 1;
+    }
+
+    return child as u32;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_content_children() {
+        let mut children = vec![1, 2, 3];
+        let mut cookies = vec![1, 2];
+        assert_eq!(2, find_content_children(&mut children, &mut cookies));
+    }
+}
