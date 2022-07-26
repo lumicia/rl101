c31a1a0bd112db371ee97f76676a6e53a4c2ef15
lumicia <zuoshengxie@outlook.com>
2022-07-26 13:09:11 +0800

    update p0135 with second solution

--- src/greedy/p0135_candy.rs
+++ src/greedy/p0135_candy.rs
@@ -1,8 +1,14 @@
+use std::cmp::max;
+
 pub fn candy(ratings: &mut Vec<u32>) -> u32 {
+    let n = ratings.len();
+    if n < 2 {
+        return n as u32;
+    }
+
     ratings.sort();
     let mut candies: u32 = 1;
     let mut total: u32 = 0;
-    let n = ratings.len();
 
     for i in 1..n {
         if ratings[i] > ratings[i - 1] {
@@ -16,6 +22,29 @@ pub fn candy(ratings: &mut Vec<u32>) -> u32 {
     total
 }
 
+pub fn candy2(ratings: &Vec<u32>) -> u32 {
+    let n = ratings.len();
+    if n < 2 {
+        return n as u32;
+    }
+
+    let mut num = vec![1; n];
+
+    for i in 1..n {
+        if ratings[i] > ratings[i - 1] {
+            num[i] = num[i - 1] + 1;
+        }
+    }
+
+    for i in (1..n).rev() {
+        if ratings[i] < ratings[i - 1] {
+            num[i - 1] = max(num[i - 1], num[i] + 1);
+        }
+    }
+
+    num.iter().sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -24,5 +53,11 @@ mod tests {
     fn test_candy() {
         let mut ratings = vec![1, 0, 2];
         assert_eq!(5, candy(&mut ratings));
+    }
+
+    #[test]
+    fn test_candy2() {
+        let mut ratings = vec![1, 0, 2];
+        assert_eq!(5, candy2(&mut ratings));
     }
 }
